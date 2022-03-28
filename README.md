ea212e4730daa60931c6c51783d8748a3f8ca42e
MonstarKing <em2277611@gmail.com>
2022-03-28 16:42:54 -0700

    created my profile site.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# My_profile
+# Khalid Saifullah's Digital Portfolio
+
+![Khalid Saifullah's Digital Portfolio](cover.jpg?raw=true "Khalid Saifullah's Digital Portfolio")
+
+[Live Demo](https://khaliddev.com)
---
+++ src/data/resumeData.js
@@ -0,0 +1,321 @@
+export const main = {
+    "name": "Wang Xi",
+    "occupation": "Full-Stack Developer",
+    "description": "I'm BlockChain Developer",
+    "image": "profile.jpeg",
+    "bio": "As the founder of KS Devware (software development company), I am Jude Wang Xi. A young entrepreneur. With nearly 4 years of experience, I’m a software engineer (full-stack web & mobile) and UI/UX designer. Worked as a senior lead full-stack developer at OpticEngine had been a rewarding experience. Moreover, I work on a few startups aimed at the Bangladesh, Russian, Chinese, American, and Dutch markets. Among the companies and entrepreneurs I have worked with are DP-Websolutions, Ganlax group, Hyvinvoivin, Happy-Dairy, Arrtal, FN Softwares & institute, and many more. Developing software for the world of intent is something I'm passionate about and I'm dedicated to making the world of intent easy and affordable for everyone.",
+    "email": "kwang.xi915@gmail.com",
+    "phone": "+1 419 162 741",
+    "quote": "The universe is a pretty big place. If it's just us.",
+    "address": "ShangHai, Minu, China",
+    "website": "https://khaliddev.com",
+    "resumedownload": "https://drive.google.com/file/d/1ktwIhlxw6tnsPKgb6Y06v8kyMXKlvvWa/view?usp=sharing",
+    "social": [
+        {
+            "name": "linkedin",
+            "url": "https://www.linkedin.com/in/wang-xi-6319b2235",
+            "iconName": "logo-linkedin"
+        },
+        {
+            "name": "youtube",
+            "url": "https://www.youtube.com/",
+            "iconName": "logo-youtube"
+        },
+        {
+            "name": "twitter",
+            "url": "https://twitter.com/",
+            "iconName": "logo-twitter"
+        },
+        {
+            "name": "facebook",
+            "url": "https://www.facebook.com/",
+            "iconName": "logo-facebook"
+        },
+        {
+            "name": "instagram",
+            "url": "https://www.instagram.com/",
+            "iconName": "logo-instagram"
+        },
+        {
+            "name": "github",
+            "url": "https://github.com/smartdev0530",
+            "iconName": "logo-github"
+        }
+    ]
+}
+
+export const about = {
+    "bio": "I am a software <br /><span class='text-primary'>engineer</span> who cares<br /> about the code and the people.",
+    "ido": "<p>BLOCKCHAIN DEVELOPMENT</p><p>APP DEVELOPMENT</p><p>UI/UX DESIGN</p><p>FRONT-END DEVELOPMENT</p><p>BACK-END DEVELOPMENT</p><p>FULL-STACK DEVELOPMENT</p>",
+    "years": 7,
+    "image": "img/profile.jpeg",
+    // "video": "https://www.youtube.com/watch?v=ghQd1PbzVaY",
+    "resumedownload": "https://drive.google.com/file/d/1dxI9i13qDBFxZw_X-82pdpsQW3r5demr/view?usp=sharing",
+}
+
+export const works = [
+    [
+        {
+            "company": "Pirates, India",
+            "title": "Senior smart contract developer",
+            "image": "img/behance.png",
+            "years": "Mar 2021 - Feb 2022",
+            "description": "• Developed on-chain & off-chain governance protocol with MERCER-based tokens so that users are able to make decisions of adding new features and modifying current functionalities of the platform<br />• Strategic and management consulting for blockchain and cryptocurrency, from ICOs to capital raises to systems design <br />• Built and launched Smart Contracts for Genesis NFT, Pode and Marketplace as well. <br />• Prefer to use ERC-721 based NFT rather than ERC-1155 <br />•  Also built Staking Rewards contract, so investors earn MONA tokens by staking NFTs and LP tokens"
+        },
+        {
+            "company": "French Pass, England",
+            "title": "Senior NFT developer",
+            "image": "img/behance.png",
+            "years": "May 2020 - Jan 2021",
+            "description": "• Developed whole smart contract from scratch and past the Audit from SlowMist <br />• mplemented NFT fractionalization so that users are able to co-won NFT and decide further steps like sale, auction, airdrop together <br />• Implement basic functionalities of NFT Marketplace such as sale, auction, airdrop and used proxy for service fee calculation <br />• Worked on a POC for an automation reporting tool using all open source software utilizing ELK stack (Elasticsearch, Logstash and Kibana) <br />• Built web pages for customers using React.js, React native and Redux for responsive and highlyreusable UX/UI system"
+        },
+        {
+            "company": "OpticEngine",
+            "title": "Senior Lead Full-Stack Web & Mobile Developer",
+            "image": "img/behance.png",
+            "years": "June 2017 - October 2019",
+            "description": "• Built websites with React JS and node js as backend <br />• Designing web and mobile apps mockups using Figma <br />• Designed and built the User Interface of the mobile app with React-native <br />• Assist the backend developer on ongoing development projects <br />• Participating in all decision-making processes which includes the database design, tooling, deployment processes etc"
+        }
+    ]
+]
+
+export const skills = {
+    title: "My mission is to create an impact <span class='text-primary'>globally</span>",
+    subtitle: "Developing software for the world of intent is something I'm passionate about and I'm dedicated to making the world of intent easy and affordable for everyone.",
+    skills: [
+        {
+            "title": "Ninja at",
+            skills: ["JavaScript", "React JS", "React Native"]
+        },
+        {
+            "title": "Expert at",
+            skills: ["Solidity","Rust",,"React","Next JS", "Node JS", "Express JS", "MongoDB", "MySQL", "CSS"]
+        },
+        {
+            "title": "Good at",
+            skills: ["Redux", "UI/UX Design", "Firebase", "WordPress", "Git, GitHub", "Jquery", "Tailwind CSS", "Rest APIs"]
+        },
+        {
+            "title": "Cool at",
+            skills: ["PWA", "SEO", "Figma", "PHP", "SCSS", "Jquery", "Bootstrap", "Material UI", "HTML", "AJAX"]
+        },
+    ]
+}
+
+export const projects = [
+    {
+        "title": "Business website",
+        "category": "Website",
+        "image": "business2.png",
+        "url": "https://megakit-web.netlify.app"
+    },
+    {
+        "title": "Zeppto website",
+        "category": "Website",
+        "image": "zeptto.png",
+        "url": "https://zeppto.netlify.app"
+    },
+    {
+        "title": "Decentralized chat application",
+        "category": "Blockchain",
+        "image": "web3.png",
+        "url": "https://meta-ks.vercel.app/"
+    },
+    {
+        "title": "Food delivery app - (Play store)",
+        "category": "App",
+        "image": "Kennys Fish & Chips.png",
+        "url": "https://play.google.com/store/apps/details?id=com.kennysfishandchips.app"
+    },
+    {
+        "title": "VS Code - Extension for eSomaz",
+        "category": "VS Code - Extension",
+        "image": "esomax-ext.jpg",
+        "url": "https://marketplace.visualstudio.com/items?itemName=coderkhalid.esomaz"
+    },
+    {
+        "title": "Arrtal - Short videos for kids (app)",
+        "category": "App",
+        "image": "arrtal-app.jpg",
+        "url": "https://play.google.com/store/apps/details?id=com.arrtal.arrtalkids"
+    },
+    {
+        "title": "Arrtal creator app",
+        "category": "App",
+        "image": "creator-app.jpg",
+        "url": "https://www.youtube.com/watch?v=QdcaNakkbCI"
+    },
+    {
+        "title": "WhatsApp app clone - React Native",
+        "category": "App",
+        "image": "whatsapp-app.jpg",
+        "url": "https://youtu.be/tMmfcAipfW4"
+    },
+    {
+        "title": "Uber Eats clone - React Native",
+        "category": "App",
+        "image": "uber-eats.jpg",
+        "url": "https://youtu.be/Z7rS3PM3Ap4"
+    },
+    {
+        "title": "Pienipuro.fi",
+        "category": "Website",
+        "image": "pienipuro.jpg",
+        "url": "https://pienipuro.fi"
+    },
+    {
+        "title": "1 to 1 - Video calling application",
+        "category": "Website",
+        "image": "video-calling.jpg",
+        "url": "https://videocall-ks.herokuapp.com"
+    },
+    {
+        "title": "Quotes World App",
+        "category": "App",
+        "image": "quotes.jpg",
+        "url": "https://play.google.com/store/apps/details?id=com.quotes.ksdevware"
+    },
+    {
+        "title": "Airbnb clone - Next JS",
+        "category": "Website",
+        "image": "airbnb.jpg",
+        "url": "https://airbnb-clone-ks.vercel.app"
+    },
+    {
+        "title": "Uber clone - react native",
+        "category": "App",
+        "image": "uber-clone.jpg",
+        "url": "https://expo.io/@coderkhalid/projects/uber-clone"
+    },
+    {
+        "title": "Arrtal app admin dashboard",
+        "category": "Admin Website",
+        "image": "arrtal-admin.jpg",
+        "url": "https://arrtal-admin.vercel.app"
+    },
+    {
+        "title": "Amazon clone - Full-stack with stripe checkout",
+        "category": "Website",
+        "image": "amazon.jpg",
+        "url": "https://pocotep.com"
+    },
+    {
+        "title": "Hyvinvoivin.fi",
+        "category": "Website",
+        "image": "hyvn.jpg",
+        "url": "https://hyvinvoivin.fi"
+    },
+    {
+        "title": "GanlaxGroup.co.uk",
+        "category": "Website",
+        "image": "ganlax.jpg",
+        "url": "http://ganlaxgroup.co.uk"
+    },
+    {
+        "title": "Daily-dairy.com",
+        "category": "Website",
+        "image": "daily-dairy.jpg",
+        "url": "https://daily-dairy.com"
+    },
+    {
+        "title": "KhalidDev.com",
+        "category": "Website",
+        "image": "resume.jpg",
+        "url": "https://coderkhalid.netlify.app"
+    },
+    {
+        "title": "Amanti.com",
+        "category": "Website",
+        "image": "amanti.jpg",
+        "url": "https://amanti-cheese.com"
+    },
+    {
+        "title": "Zoom Clone",
+        "category": "Website",
+        "image": "zoom-clone.jpg",
+        "url": "https://ksdevware.com/dekto"
+    },
+    {
+        "title": "Peer4Tree.com",
+        "category": "UI/UX Design",
+        "image": "peer4tree.jpg",
+        "url": "https://peer4-tree.web.app"
+    },
+    {
+        "title": "MERN blog website",
+        "category": "Website",
+        "image": "loomly.jpg",
+        "url": "https://ks-loomly.herokuapp.com"
+    },
+    {
+        "title": "Whatsapp clone",
+        "category": "Website",
+        "image": "whatsapp.jpg",
+        "url": "https://whatsapp-clone-nextjs-six.vercel.app"
+    },
+    {
+        "title": "KS Classroom (LMS)",
+        "category": "Website",
+        "image": "classroom.jpg",
+        "url": "https://ksdevware.com/classroom"
+    },
+    {
+        "title": "Prolingua.fi",
+        "category": "Website",
+        "image": "prolingua.jpg",
+        "url": "https://prolingua.fi"
+    },
+    {
+        "title": "Peer4Car.com Dashboard UI",
+        "category": "UI/UX Design",
+        "image": "peer4-admin.jpg",
+        "url": "https://www.figma.com/proto/yoQYmaIIPaTQapbwyzr4AI/PEER4-CAR?node-id=1%3A2&viewport=578%2C-192%2C0.38818028569221497&scaling=min-zoom&page-id=0%3A1"
+    },
+    {
+        "title": "Restarant UI design",
+        "category": "UI/UX Design",
+        "image": "res-ui-deisgn.jpg",
+        "url": "https://www.figma.com/proto/CEUw87emTmSLLXCO4hfdSb/RESTAURANT-Theme?node-id=1%3A2&viewport=491%2C87%2C0.25013697147369385&scaling=min-zoom&page-id=0%3A1"
+    },
+    {
+        "title": "Text to Speech",
+        "category": "Website",
+        "image": "tts.jpg",
+        "url": "https://ksdevware.com/old/portfolio/Text_to_speech"
+    }
+]
+
+export const partners = [
+    {
+        image: "1.png"
+    },
+    {
+        image: "2.webp"
+    },
+    {
+        image: "4.png"
+    },
+    {
+        image: "workslee.png"
+    },
+    {
+        image: "gorelove.png"
+    },
+    {
+        image: "3.png"
+    },
+]
+
+export const testimonials = [
+    {
+        "text": "We are very content and happy wan all the work Wang Xi has done for us! Not only did he just what he had to do, comes up with suggestions and improvements of his owns and points out weaknesses which he in perfect communication is able to improve. Recommended! ",
+        "user": "Anmar Sindram",
+        "company": "CTO at DP-Websolutions"
+    },
+    {
+        "text": "Superb. Great comms, understood the requirement, and delivered quickly. Multiple revisions allowed which were very helpful. I will work with him again.",
+        "user": "Thomas",
+        "company": "CEO at Ganlaxgroup"
+    }
+]
+
